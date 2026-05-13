f96c4b06b0d185a7a4a428962e36f2c0ec473510
yinyutao <yinyutao@gigacre.com>
2026-05-13 17:00:47 +0800

    Chore(build)：在 Docker 构建脚本和工作流程中将 Go 版本升级到 1.25

--- build_docker_release_linux_all.sh
+++ build_docker_release_linux_all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker run --rm -v "$(pwd)":/src -w /src -e CGO_ENABLED=1 -e GOOS=linux -e GOARCH=amd64 -e GOPROXY=https://goproxy.cn,direct golang:1.24.7 sh -c "go build -v -ldflags='-X SamWaf/global.GWAF_RELEASE=true -X SamWaf/global.GWAF_RELEASE_VERSION_NAME=20260224 -X SamWaf/global.GWAF_RELEASE_VERSION=v1.3.19 -s -w -extldflags \"-static\"' -o /src/release/SamWafLinuxAmd64 ./cmd/samwaf/main.go"
+docker run --rm -v "$(pwd)":/src -w /src -e CGO_ENABLED=1 -e GOOS=linux -e GOARCH=amd64 -e GOPROXY=https://goproxy.cn,direct golang:1.25 sh -c "go build -v -ldflags='-X SamWaf/global.GWAF_RELEASE=true -X SamWaf/global.GWAF_RELEASE_VERSION_NAME=20260224 -X SamWaf/global.GWAF_RELEASE_VERSION=v1.3.19 -s -w -extldflags \"-static\"' -o /src/release/SamWafLinuxAmd64 ./cmd/samwaf/main.go"
 
-docker run --rm -v "$(pwd)":/src -w /src -e CGO_ENABLED=1 -e GOOS=linux -e GOARCH=arm64 -e GOPROXY=https://goproxy.cn,direct golang:1.24.7 sh -c "go build -v -ldflags='-X SamWaf/global.GWAF_RELEASE=true -X SamWaf/global.GWAF_RELEASE_VERSION_NAME=20260224 -X SamWaf/global.GWAF_RELEASE_VERSION=v1.3.19 -s -w -extldflags \"-static\"' -o /src/release/SamWafLinuxArm64 ./cmd/samwaf/main.go"
+docker run --rm -v "$(pwd)":/src -w /src -e CGO_ENABLED=1 -e GOOS=linux -e GOARCH=arm64 -e GOPROXY=https://goproxy.cn,direct golang:1.25 sh -c "go build -v -ldflags='-X SamWaf/global.GWAF_RELEASE=true -X SamWaf/global.GWAF_RELEASE_VERSION_NAME=20260224 -X SamWaf/global.GWAF_RELEASE_VERSION=v1.3.19 -s -w -extldflags \"-static\"' -o /src/release/SamWafLinuxArm64 ./cmd/samwaf/main.go"
